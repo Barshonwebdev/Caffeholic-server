4f2875b431def3183f5c2500126e1c8a30c99aa0
Barshon <barshonweb@gmail.com>
2024-06-03 01:57:26 +0600

    mongodb database connected

--- index.js
+++ index.js
@@ -5,10 +5,39 @@ const cors=require('cors');
 require('dotenv').config();
 const jwt=require('jsonwebtoken');
 const port=process.env.PORT||5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+// mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_CLUSTER_URL}`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      
+    }
+  }
+  run().catch(console.dir);
+
 
 app.get('/',(req,res)=>{
     res.send('running');
