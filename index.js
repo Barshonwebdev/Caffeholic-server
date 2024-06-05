ecdc86e97a1d65720380f16421015481b39d7f2f
Barshon <barshonweb@gmail.com>
2024-06-05 18:19:59 +0600

    user data get api

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // server node packages requires
-const express=require('express');
-const app=express();
-const cors=require('cors');
-require('dotenv').config();
-const jwt=require('jsonwebtoken');
-const port=process.env.PORT||5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middlewares
 app.use(cors());
@@ -20,18 +20,18 @@ function createToken(user) {
     "secret",
     { expiresIn: "7d" }
   );
-  
+
   return token;
 }
 
-// jwt token verify function 
-function verifyToken(req,res,next){
-  const token=req.headers.authorization.split(' ')[1];
-  const verify=jwt.verify(token,"secret");
-  if(!verify?.email){
-   return res.send('You are not authorized');
+// jwt token verify function
+function verifyToken(req, res, next) {
+  const token = req.headers.authorization.split(" ")[1];
+  const verify = jwt.verify(token, "secret");
+  if (!verify?.email) {
+    return res.send("You are not authorized");
   }
-  req.user=verify.email;
+  req.user = verify.email;
   console.log(verify);
   next();
 }
@@ -40,70 +40,69 @@ function verifyToken(req,res,next){
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_CLUSTER_URL}`;
 
-
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-  async function run() {
-    try {
-      client.connect();
-      
+async function run() {
+  try {
+    client.connect();
 
-      const caffeholicDB=client.db('caffeholic_db');
-      const coffeepostsCollection=caffeholicDB.collection('coffeepost_collection');
-      const userCollection=caffeholicDB.collection('caffeholic_users');
+    const caffeholicDB = client.db("caffeholic_db");
+    const coffeepostsCollection = caffeholicDB.collection(
+      "coffeepost_collection"
+    );
+    const userCollection = caffeholicDB.collection("caffeholic_users");
 
-      // user APIs
+    // user APIs
 
     app.post("/user", async (req, res) => {
       const user = req.body;
-      const token=createToken(user);
+      const token = createToken(user);
       console.log(token);
       const isUserExist = await userCollection.findOne({ email: user.email });
       if (isUserExist) {
         return res.send({
           message: "user already exists on database",
-          token
+          token,
         });
       }
       const result = await userCollection.insertOne(user);
-      res.send({token});
+      res.send({ token });
     });
 
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await userCollection.findOne({ email });
+      res.send(result);
+    });
 
-      // coffeeposts api 
+    // coffeeposts api
 
-      // add product
     app.post("/coffeeposts", verifyToken, async (req, res) => {
       const postData = req.body;
       const result = await coffeepostsCollection.insertOne(postData);
       res.send(result);
     });
 
-      app.get('/coffeeposts',async(req,res)=>{
-        const result=await coffeepostsCollection.find().toArray();
-        res.send(result); 
-      })
-
-      
-    } finally {
-      // Ensures that the client will close when you finish/error
-      
-    }
+    app.get("/coffeeposts", async (req, res) => {
+      const result = await coffeepostsCollection.find().toArray();
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("running");
+});
 
-app.get('/',(req,res)=>{
-    res.send('running');
-})
-
-app.listen(port,()=>{
-    console.log(`listening at port: ${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`listening at port: ${port}`);
+});
